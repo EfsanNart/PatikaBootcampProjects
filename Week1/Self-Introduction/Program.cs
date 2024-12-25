2a662d63fdeb8c8a7987352a795c04182e24cc1a
Efşan Nart <74191913+EfsanNart@users.noreply.github.com>
2024-12-25 14:36:06 +0300

    Self-Introduction projesi tamamlandı

--- Week1/Self-Introduction/Program.cs
+++ Week1/Self-Introduction/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; // Bu  ifade system ad alanını programa dahil eder, bu ad alanı temel .Net sınıflarını içerir
+
+namespace SelfIntroduction //namespace kodu düzenlemek ve isim çakışmalarını önlemek için kullanılan kapsayıcıdır.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Adımızı soyadımızı erkana yazdırıyoruz
+            Console.WriteLine("Efşan Nart");
+            //Bootcampde olma sebebimizi açıklıyoruz
+            Console.WriteLine("Yazılım alanında kendimi geliştirmek ve kariyerime yeni bir yön vermek için.");
+
+        }
+    }
+}
