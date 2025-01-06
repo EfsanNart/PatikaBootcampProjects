790b33e657816ccd836fc4a3855edf8120d31c97
Efşan Nart <74191913+EfsanNart@users.noreply.github.com>
2025-01-06 10:03:12 +0300

    completed

---
+++ Week4/CreateClassWithOOP/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClassWithOOP
+{
+    internal class Person
+    {
+        /* ------------------------------------------------------------------------------------------------------------------------------------------------
+         Eğer Ad zaten public bir property olarak tanımlıysa ve doğrudan erişilebilir durumdaysa, 
+         bir property'den başka bir property'yi döndürmek genellikle gerekmez örneğin 
+
+         public string Ad { get; set; }
+         public string Isim
+        {
+          get { return Ad; } // Ad property’sini döndür
+          set { Ad = value; } // Ad property’sine değer ata } Bu yapı çalışır, ama gereksizdir çünkü Ad zaten erişilebilir durumdadır.
+        ----------------------------------------------------------------------------------------------------------------------------------------------------*/
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string Branş {  get; set; }
+        public string Sınıf {  get; set; }
+
+        public void Ogretmenİnfo()
+        {
+            Console.WriteLine($"{Ad} {Soyad} adlı öğretmenin doğum tarihi : {DogumTarihi:dd.MM.yyyy} 'dir ve branşı {Branş}'dır.");
+        }
+        public void Ogrenciİnfo()
+        {
+            Console.WriteLine($"{Ad} {Soyad} adlı öğrencinin doğum tarihi : {DogumTarihi:dd.MM.yyyy} 'dir ve {Sınıf} sınıfındadır.");
+        }
+    }
+}
---
+++ Week4/CreateClassWithOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using CreateClassWithOOP;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Person ögrenci = new Person();
+        ögrenci.Ad = "Efşan";
+        ögrenci.Soyad = "Nart";
+        ögrenci.DogumTarihi = new DateTime(2001, 3, 17);
+        ögrenci.Sınıf = "Back-End Web Developer ";
+        ögrenci.Ogrenciİnfo();
+
+
+
+        Person ögretmen = new Person();
+        ögretmen.Ad = "Sertan";
+        ögretmen.Soyad = "Bozkuş";
+        ögretmen.DogumTarihi = new DateTime(1996, 7, 12);
+        ögretmen.Branş = "Yazılım";
+        ögretmen.Ogretmenİnfo();
+    }
+}
