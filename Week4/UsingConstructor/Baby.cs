ad2d6da4fe06c7612e3e12655630c79e38fbe5b6
Efşan Nart <74191913+EfsanNart@users.noreply.github.com>
2025-01-07 15:58:32 +0300

    completed

---
+++ Week4/UsingConstructor/Baby.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UsingConstructor
+{
+    internal class Baby
+    {
+        public string _name;
+        public string _surname;
+        public DateTime _birthDate;
+
+        //Default contructor
+        public Baby()
+        {   //OverLoading 
+            //Eğer özel bir constructor tanımlarsak ve boş bir new ile kullanılacak constructora ihtiyaç duyarsak bunu tanımlarız
+            _name = "Unknown";
+            _surname = "Unknown";
+            BirthProcess();
+        }
+        //Parametreli constructor
+        public Baby(string name, string surname)
+        { 
+            _name = name;
+            _surname = surname;
+            BirthProcess();
+        }
+        public void BirthProcess()
+        {    //2 Constructor içinde ortak olarak bebek nesnesi oluşturulduğunda konsol ekranında bir " Ingaaaa " yazısı görülsün ve Doğum Tarihi o an olarak atansın.
+            _birthDate = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+    }
+}
--- Week4/UsingConstructor/Program.cs
+++ Week4/UsingConstructor/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//projeyi henüz yapmadım 
+﻿using UsingConstructor;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Default Constructor kullanımı
+        Baby baby1 = new Baby();
+        baby1._name = "Ron";
+        baby1._surname = "Weasley";
+        Console.WriteLine($"Bebek 1: Ad={baby1._name}, Soyad={baby1._surname}, Doğum Tarihi={baby1._birthDate}");
+
+        //  Parametreli Constructor kullanımı
+        Baby baby2 = new Baby("Elif", "Aybal");
+        Console.WriteLine($"Bebek 2: Ad={baby2._name}, Soyad={baby2._surname}, Doğum Tarihi={baby2._birthDate}");
+
+        Console.ReadLine(); // Konsolun açık kalması için
+    }
+}
