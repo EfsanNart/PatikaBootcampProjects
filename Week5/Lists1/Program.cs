6cad56807827a265c3ede88c8d024436887d9eba
Efşan Nart <74191913+EfsanNart@users.noreply.github.com>
2025-01-17 10:39:44 +0300

    completed

---
+++ Week5/Lists1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> davetliler = new List<string>()
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        //Listeyi yazdırma
+        Console.WriteLine("Davetliler:");
+        foreach (var davetli in davetliler)
+        {
+            
+            Console.WriteLine(davetli);
+        }
+    }
+}
+
