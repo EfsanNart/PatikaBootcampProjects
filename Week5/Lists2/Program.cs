c96d671114dcb93becb2d8b56aed7b0cb8c57ff3
Efşan Nart <74191913+EfsanNart@users.noreply.github.com>
2025-01-17 10:58:30 +0300

    done

---
+++ Week5/Lists2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> coffees = new List<string>();
+
+        Console.Write("Kaç kahve ismi girmek istiyorsunuz? ");
+        int adet = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < adet; i++)
+        {
+            string coffe;
+            while (true)
+            {
+                Console.Write($"Kahve {i + 1} : ");
+                coffe = Console.ReadLine();
+
+                // Sayı içeriyorsa tekrar giriş iste
+                if (ContainsNumber(coffe))
+                {
+                    Console.WriteLine("⚠ Lütfen sadece harf içeren bir kahve ismi girin!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            coffees.Add(coffe);
+        }
+
+        // Listeyi yazdır
+        Console.WriteLine("\nGirilen Kahve İsimleri:");
+        foreach (var coffe in coffees)
+        {
+            Console.WriteLine(coffe);
+        }
+    }
+
+    // İçinde rakam var mı kontrol eden fonksiyon
+    static bool ContainsNumber(string input)
+    {
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))return true;
+        }
+        return false;
+    }
+}
