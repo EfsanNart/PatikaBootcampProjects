4f70494c74b234439f2e8392973cb23221ccc683
Efşan Nart <74191913+EfsanNart@users.noreply.github.com>
2025-02-02 20:56:35 +0300

    completed

---
+++ Week7/SchoolDatabase/Models/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDatabase.Models
+{
+    internal class Class
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public Class(int classId,string className)
+        {
+            ClassId = classId;
+            ClassName = className;
+        }
+    }
+}
---
+++ Week7/SchoolDatabase/Program.cs
@@ -0,0 +1,44 @@
+﻿using SchoolDatabase.Models;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student(1,"Ali",1),
+            new Student(2,"Ayşe",2),
+            new Student(3,"Mehmet",1),
+            new Student(4,"Fatma",3),
+            new Student(5,"Ahmet",2)
+        };
+        List<Class> classes = new List<Class>
+        {
+            new Class(1,"Matematik"),
+            new Class(2,"Türkçe"),
+            new Class(3,"Kimya")
+        };
+        var groupedData = from cls in classes //classes koleksiyonundan her bir sınıfı (cls) alır.
+                          join student in students on cls.ClassId equals student.ClassId into studentGroup //students tablosu ile classes tablosunu ClassId üzerinden birleştirir,Aynı ClassId'ye sahip tüm öğrencileri studentGroup adlı bir koleksiyona gruplandırır
+                          select new
+                          {
+                              ClsName = cls.ClassName,
+                              Students = studentGroup
+                          };
+        // Sonuçları ekrana yazdırma
+        foreach (var item in groupedData)
+        {
+            Console.WriteLine($" {item.ClsName} Dersini Alanlar:");
+
+            foreach (var student in item.Students)
+            {
+                Console.WriteLine($" {student.StudentName}");
+            }
+
+            Console.WriteLine(); // Boşluk bırak
+
+
+        }
+    }
+}
